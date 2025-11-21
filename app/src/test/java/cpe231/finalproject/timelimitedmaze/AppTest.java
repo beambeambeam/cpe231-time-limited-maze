481f0badcea6f7e845c9b4343f454693165dda55
beambeambeam <supawitmarayat@gmail.com>
2025-11-21 18:20:33 +0700

    Add Placeholder test

--- app/src/test/java/cpe231/finalproject/timelimitedmaze/AppTest.java
+++ app/src/test/java/cpe231/finalproject/timelimitedmaze/AppTest.java
@@ -1,4 +1,13 @@
 package cpe231.finalproject.timelimitedmaze;
 
-class AppTest {
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+final class AppTest {
+
+  @Test
+  void appExposesMainMethod() throws NoSuchMethodException {
+    assertNotNull(App.class.getDeclaredMethod("main", String[].class));
+  }
 }
